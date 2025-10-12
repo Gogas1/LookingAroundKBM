30542393f7e6117a9bba1afba00141a50bde66e5
Никита Каргапольцев <kargnikita5@gmail.com>
2025-10-12 11:25:37 +0200

    Update thunderstore files, code cleanup

--- Source/HarmonyPatches/PlayerCameraPanControllerPatches.cs
+++ Source/HarmonyPatches/PlayerCameraPanControllerPatches.cs
@@ -13,9 +13,6 @@ namespace LookingAroundKBM.HarmonyPatches {
         static FieldInfo f_lastLookDirection = AccessTools.Field(typeof(PlayerCameraPanController), "lastLookDirection");
         static FieldInfo f_lookFurtherDirection = AccessTools.Field(typeof(PlayerCameraPanController), "lookFurtherDirection");
 
-        static bool isLooking = false;
-        static Vector2 anchorMousePosition = Vector2.zero;
-
         static float maxOffset = 96f * 1;
         static float horizontalModifier = 1f;
         static float verticalModifier = 1f;
@@ -35,10 +32,6 @@ namespace LookingAroundKBM.HarmonyPatches {
 
             Vector2 vector = Vector2.zero;
             if (LookingAroundKBM.cameraControllKeybind.Value.IsPressed() && Player.i.CanPanCamera) {
-                if(!isLooking) {
-                    isLooking = true;
-                    anchorMousePosition = Input.mousePosition;
-                }
 
                 var width = Screen.width;
                 var height = Screen.height;
@@ -46,11 +39,7 @@ namespace LookingAroundKBM.HarmonyPatches {
                 var verticalFactor = (Input.mousePosition.y - height / 2) / (height / 2);
                 var horizontalFactor = (Input.mousePosition.x - width / 2) / (width / 2);
 
-                var offset = anchorMousePosition - (Vector2)Input.mousePosition;
                 vector += 1 * new Vector2(horizontalFactor * maxOffset * horizontalModifier, verticalFactor * maxOffset * verticalModifier);
-            }
-            else {
-                isLooking = false;
             }
 
             f_lookFurtherDirection.SetValue(__instance, vector);
